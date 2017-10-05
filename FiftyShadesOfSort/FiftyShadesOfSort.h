986826bccca8a6a1ef87fc8ca0b459e4008540eb
Vjacheslav Volodko <killobatt@gmail.com>
2017-10-05 23:17:11 +0200

    Initial Commit

---
+++ FiftyShadesOfSort/FiftyShadesOfSort.h
@@ -0,0 +1,19 @@
+//
+//  FiftyShadesOfSort.h
+//  FiftyShadesOfSort
+//
+//  Created by Vjacheslav Volodko on 05.10.17.
+//  Copyright © 2017 FiftyShadesOfSort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FiftyShadesOfSort.
+FOUNDATION_EXPORT double FiftyShadesOfSortVersionNumber;
+
+//! Project version string for FiftyShadesOfSort.
+FOUNDATION_EXPORT const unsigned char FiftyShadesOfSortVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FiftyShadesOfSort/PublicHeader.h>
+
+
